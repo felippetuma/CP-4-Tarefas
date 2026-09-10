2a6b98cca7c353bc232b5a5db5564f7b6cd38afe
Felippe Tuma <felippetuma@outlook.com>
2026-09-10 15:08:45 -0300

    feat: adiciona componentes principais e botão com acesso ao modal para adicionar tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import Aside from "./components/Aside";
+import ButtonTarefa from "./components/ButtonTarefa";
+import Main from "./components/Main";
 
 const App = () => {
   return (
-    <>
-    
-    </>
+    <div className="flex min-h-screen min-w-screen mx-auto bg-zinc-800">
+      <Aside>
+      </Aside>
+        <Main>
+         <ButtonTarefa/>
+        </Main>
+    </div>
   )
 }
+
 
 export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = ({ children }) => {
+  return (
+    <div className="flex-1 px-8 pt-8 pb-20 flex items-start justify-end">
+      {children}
+    </div>
+  )
+}
+
+export default Main
