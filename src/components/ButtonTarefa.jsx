54b64590a187925af8c450286d0d019fe6b8b4fb
Felippe Tuma <felippetuma@outlook.com>
2026-09-11 15:12:06 -0300

    feat(ButtonTarefa.jsx): adiciona o salvamento das tarefas enviadas, filtos e remover o item

--- src/components/ButtonTarefa.jsx
+++ src/components/ButtonTarefa.jsx
@@ -1,12 +1,70 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { IconAdd } from "./Icons";
+import ListaTarefas from "./ListaTarefas";
+import Filtros from "./Filtros";
 
 const ButtonTarefa = () => {
     const [open, setOpen] = useState(false);
+    const [filtros, setFiltros] = useState("todas");
+    const [tarefas, setTarefas] = useState(() => {
+        const salvarTarefas = localStorage.getItem("tarefas");
+        return salvarTarefas ? JSON.parse(salvarTarefas) : [];
+    })
+
+    const [formulario, setFormulario] = useState({
+        nome: "",
+        data: "",
+        descricao: "",
+        prioridade: "baixa",
+    });
+
+    useEffect(() => {
+        localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    }, [tarefas]);
+
+    const adicionarTarefa = (e) => {
+        e.preventDefault();
+
+        if(!formulario.nome.trim()) return;
+
+        const novaTarefa = {
+            id: Date.now(),
+            ...formulario,
+            completed: false
+        };
+
+        setTarefas([...tarefas, novaTarefa]);
+        setFormulario({
+            nome: "", 
+            data: "",
+            descricao: "",
+            prioridade: ""
+        })
+        setOpen(false);
+    };
+
+    const alternarConclusao = (id) => {
+        const conclusaoTarefa = tarefas.map((tarefas => (
+            tarefas.id === id ? {...tarefas, completed: !tarefas.completed } : tarefas
+        )))
+        setTarefas(conclusaoTarefa);
+    };
+
+    const removerTarefa = (id) => {
+        const apagarTarefa = tarefas.filter((tarefas) => tarefas.id != id);
+        setTarefas(apagarTarefa);
+    };
+
+    const filtrarTarefas = tarefas.filter((tarefas) => {
+        if(filtros === "pendentes") return !tarefas.completed;
+        if(filtros === "concluidas") return tarefas.completed;
+
+        return true;
+    });
 
     return (
-        <div>
-            <button onClick={() => setOpen(true)} className="rounded-[43px] mx-auto bg-green px-4 py-4 text-black flex justify-center items-center gap-3 text-lg">
+        <div className="mt-8 flex w-full max-w-3xl flex-col items-center">
+            <button onClick={() => setOpen(true)} className="flex items-center justify-center gap-3 rounded-[43px] bg-green px-4 py-4 text-lg text-black">
                 <IconAdd /> Adicionar Tarefa
             </button>
 
@@ -14,33 +72,39 @@ const ButtonTarefa = () => {
                 <div className="fixed inset-0 flex items-center justify-center bg-black/70">
                     <div className="rounded-3xl bg-zinc-800 text-green p-10 mx-auto">
                         <h2 className="text-xl font-bold text-center">Adicionar Tarefa</h2>
-                        <form action="" className="flex flex-col gap-4">
+                        <form action="" onSubmit={adicionarTarefa} className="flex flex-col gap-4">
                             <p>Nome: </p>
-                            <input type="text" className="px-5 py-2 mt-2 min-w-120" placeholder="Digite uma nova tarefa." />
+                            <input type="text" value={formulario.nome} className="px-5 py-2 mt-2 min-w-120" onChange={(e) => setFormulario({...formulario, nome: e.target.value})} placeholder="Digite uma nova tarefa." />
                             <div className="flex gap-30">
                                 <div className="flex  flex-col gap-8">
-
-                                <p>Data:</p>
-                                <input type="date" className="" />
+                                    <p>Data:</p>
+                                    <input type="date" className="outline-none " value={formulario.data} onChange={(e) => setFormulario({...formulario, data: e.target.value})}/>
                                 </div>
-                                 <div className="flex  flex-col gap-8">
+                                <div className="flex  flex-col gap-8">
 
-                                <p>Prioridade:</p>
-                                <select name="" id="">
-                                    <option value="baixa" key="">Baixa</option>
-                                    <option value="media" key="">Media</option>
-                                    <option value="alta" key="">Alta</option>
-                                </select>
-                                 </div>
+                                    <p>Prioridade:</p>
+                                    <select className="bg-zinc-800 " name="" value={formulario.prioridade} onChange={(e) => setFormulario({...formulario, prioridade: e.target.value})}>
+                                        <option value="baixa" key="">Baixa</option>
+                                        <option value="media" key="">Media</option>
+                                        <option value="alta" key="">Alta</option>
+                                    </select>
+                                </div>
                             </div>
+                            <p>Descrição</p>
+                            <input type="text" value={formulario.descricao} onChange={(e) => setFormulario({...formulario, descricao: e.target.value})} className="px-10 py-10 min-w-120 min-h-10" placeholder="Digite a descrição do projeto"/>         
+                                   
                             <div className=" flex gap-4 p-4 justify-end">
-                                <button onClick={() => setOpen(false)}>Fechar</button>
-                                <button onClick={() => setOpen(false)} className="text-zinc-800 bg-green rounded-2xl px-8 py-2 text-lg ">Salvar</button>
+                                <button type="button" onClick={() => setOpen(false)}>Fechar</button>
+                                <button type="submit" className="text-zinc-800 bg-green rounded-2xl px-8 py-2 text-lg ">Salvar</button>
                             </div>
                         </form>
                     </div>
                 </div>
             )}
+            <div className="mt-8 w-full">
+                <Filtros filtroAtual={filtros} onChange={setFiltros}/>
+            </div>
+            <ListaTarefas tarefas={filtrarTarefas} onToggle={alternarConclusao} onRemove={removerTarefa}/>
         </div>
     )
 }
