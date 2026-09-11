d01e703fd1001ac2e185c7cb24cc32162bd730dd
Yan <yan2008lima@gmail.com>
2026-09-11 10:28:43 -0300

    feat: adiciona componente de lista de tarefas

---
+++ src/components/ListaTarefas.jsx
@@ -0,0 +1,16 @@
+import CardTarefa from "./CardTarefa"
+
+const ListTarefas = ({ tarefas, onToggle, onRemove }) => {
+    if (tarefas.length === 0) {
+        return <p className='mt-6 flex text-zinc-400 '> Nenhuma Tarefa encontrada</p>
+    }
+    return (
+        <section className='mt-4 w-full space-y-4'>
+            {tarefas.map((tarefas) => (
+                <CardTarefa key={tarefas.id} tarefas={tarefas} onToggle={onToggle} onRemove={onRemove} />
+            ))}
+        </section>
+    )
+}
+
+export default ListTarefas
