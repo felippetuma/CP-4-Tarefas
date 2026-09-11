8d3855aa59ca67e33887adb2aa30f3c0474f5a59
Felippe Tuma <felippetuma@outlook.com>
2026-09-11 14:50:07 -0300

    style(Main): modifica o estilo utilizado no componente Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 const Main = ({ children }) => {
   return (
-    <div className="flex-1 px-8 pt-8 pb-20 flex items-start justify-end">
+    <div className="relative flex flex-1 flex-col items-center px-8 pt-8 pb-20">
       {children}
     </div>
   )
