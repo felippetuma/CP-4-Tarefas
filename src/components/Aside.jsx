ce47ca41f4185a4c09cfe0ac11a85f0ae425fe82
Felippe Tuma <felippetuma@outlook.com>
2026-09-11 15:03:17 -0300

    feat(Aside.jsx): remove o componente Aside

--- src/components/Aside.jsx
+++
@@ -1,9 +0,0 @@
-const Aside = ({ children }) => {
-  return (
-    <aside className="w-72 shrink-0 bg-green min-h-screen py-20 px-10 flex flex-col gap-14 rounded-lg">
-      {children}
-    </aside>
-  )
-}
-
-export default Aside
